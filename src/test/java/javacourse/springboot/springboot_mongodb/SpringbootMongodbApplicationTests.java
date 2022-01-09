bea1125f48c0ccdb798c09c46f49f0a449f792a9
Leonardo Barboza Borges <54484227+leonbborges@users.noreply.github.com>
2022-01-09 18:34:50 -0300

    Initial commit

---
+++ src/test/java/javacourse/springboot/springboot_mongodb/SpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package javacourse.springboot.springboot_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
